eb8d908f8220bcd282bdb0709cd9f10d66a660c3
yukihiro <etuning729@gmail.com>
2010-10-11 19:49:31 +0900

    second commit

---
+++ rojiura_garden.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<link rel ="stylesheet" href ="css/common.css" type="text/css" />
+<title>山野智子 フルート</title>
+</head>
+<body id="tomoko-wpj">
+<div id="wrapper">
+	<div id="contents">
+		<!--金縁(フルート調の)をつけてみたり,散らしてみたり-->
+        <?php include 'element/common/header.php';?>
+		<div id="main-feature">
+        <?php include 'element/common/contents_menu.php';?>
+		<?php
+            $no = $_REQUEST["no"];
+			include "element/rojiura/garden/menu.php";
+            include "element/rojiura/garden/contents.php";
+		?>
+		</div>
+		<div id="side-menu">
+        <a href="admin/index.php">管理者</a>
+		</div>
+		<div id="recent-news">
+		</div>
+	</div>
+</div>
+<?php include 'element/common/footer.php';?>
+</body>
+</html>
